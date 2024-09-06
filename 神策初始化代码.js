bc5e7eb3b32189f4a7495788dd26e23407e22ce9
oxiangyang <908581983@qq.com>
2024-09-06 10:41:39 +0800

    Create 神策初始化代码.js

---
+++ 神策初始化代码.js
@@ -0,0 +1,39 @@
+ //该方法在app.js中，在启动时会多次调用
+
+// 测试1
+ getToken() {
+    return new ZMPromise((resolve, reject) => {
+      this.request({
+        url: url,
+        data: {},
+        noLoading: true,
+        method: 'GET',
+        success: async res => {
+          const isMember = Boolean(res.code === 0 && res.data);
+          const properties = { is_member_status: isMember };
+          registerProperties.hookRegister(function ({event, props, data}) {
+            return properties;
+          });
+          sensors.registerApp(properties);
+          if (isMember) {
+            // 迪卡侬会员
+            // 设置personid
+            sensors.identify(res.data.unionid, true);
+            sensors.login(res.data.person_id);
+          } else {
+            // 拿不到personid则设置unionid
+            const unionId = wx.getStorageSync('union_id');
+            sensors.identify(unionId, true);
+          }
+          resolve(res);
+        },
+        fail(err) {
+          reject(err);
+        },
+        complete(resp) {
+          // 需要在sensors.identify()后执行，否则神策会定义uuid()为distinct_id，无法将用户关联
+          sensors.init();
+        },
+      });
+    })
+  },
